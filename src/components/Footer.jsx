babc47c1b13c7319879722083b794b7a2c0491c4
Santiago Ledesma <ledesmasantiago10@gmail.com>
2025-04-20 13:35:11 -0300

    Se modificó el diseño y se agregó animaciones a todos las secciones

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,29 @@ import "../styles/Footer.css";
 function Footer() {
   return (
     <footer className="footer" id="footer">
+      <div className="social-buttons">
+        <a
+          href="https://www.linkedin.com/in/santiago-esteban-ledesma-a661b32a5/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="btn-social linkedin"
+        >
+          LinkedIn
+        </a>
+        <a
+          href="https://github.com/santled"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="btn-social github"
+        >
+          GitHub
+        </a>
+      </div>
       <p>© 2025 Juan Pérez. Todos los derechos reservados.</p>
     </footer>
   );
 }
 
 export default Footer;
+
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,31 @@ import "../styles/Header.css";
 function Header() {
   return (
     <header className="header">
-      <h1>¡Hola! Soy Juan Pérez</h1>
-      <p>Desarrollador Frontend apasionado por la tecnología y el diseño web.</p>
+      <div className="seccion">
+        <div className="profile">
+          <img
+            src="/Assets/Images/Mi_foto.jpg"
+            alt="Foto de perfil"
+            className="profile-image"
+          />
+          <div className="profile-text">
+            <h1>LEDESMA SANTIAGO</h1>
+            <p>Estudiante de Programación</p>
+            <div className="about-me">
+            <h2 className="subtitulo">Sobre mí</h2>
+              <p>
+                Soy una persona apasionada por el desarrollo de software, con ganas de seguir aprendiendo y creciendo en el mundo de la tecnología.
+                Actualmente me encuentro estudiando programación en la UTN y desarrollando proyectos personales para mejorar mis habilidades.
+              </p>
+            </div>
+            
+          </div>
+        </div>
+      </div>
     </header>
   );
 }
 
 export default Header;
-
 
 
